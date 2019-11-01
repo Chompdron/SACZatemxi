21097ab4ca954780ea6b7900ae613167aa73f1ee
mill120197 <53064397+mill120197@users.noreply.github.com>
2019-10-31 20:13:23 -0600

    base
    Lo de la semana pasada

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -38,10 +38,19 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Saludos', 'url' => ['/site/saludo']],
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'Clientes', 'url' => ['/cliente/index']],
+            ['label' => 'Insumo', 'url' => ['/insumo/index']],
+            ['label' => 'Pedido', 'url' => ['/pedido/index']],
+            ['label' => 'Pedido Proveedor', 'url' => ['/pedido-prov/index']],
+            ['label' => 'Pedido proveedor lista', 'url' => ['/pedido-prov-lista/index']],
+            ['label' => 'Producto', 'url' => ['/producto/index']],
+            ['label' => 'Proveedor', 'url' => ['/proveedor/index']],
+            ['label' => 'Receta', 'url' => ['/receta/index']],
+            ['label' => 'Roles', 'url' => ['/role/index']],
+            ['label' => 'Unidad de Presentacion', 'url' => ['/unidad-presentacion/index']],
+            ['label' => 'Usuarios', 'url' => ['/users/index']],
+            ['label' => 'Venta', 'url' => ['/venta/index']],
+            ['label' => 'Venta Lista', 'url' => ['/venta-lista/index']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
@@ -56,10 +65,18 @@ AppAsset::register($this);
             )
         ],
     ]);
+        
     NavBar::end();
     ?>
 
     <div class="container">
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
