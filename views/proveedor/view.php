efc96e82f08cf27839ad9dc179f7f396388694ae
Xavicamach <xavicamach@users.noreply.github.com>
2019-11-07 20:59:59 -0600

    Traducción Views

--- views/proveedor/view.php
+++ views/proveedor/view.php
@@ -16,11 +16,11 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->ProveedorID], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->ProveedorID], [
+        <?= Html::a('Actualizar', ['update', 'id' => $model->ProveedorID], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Borrar', ['delete', 'id' => $model->ProveedorID], [
             'class' => 'btn btn-danger',
             'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
+                'confirm' => '¿Estás seguro que deseas borrarlo?',
                 'method' => 'post',
             ],
         ]) ?>
