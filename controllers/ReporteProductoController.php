059ffb01a77da404b73549eccb7219f3bbc82faa
angelmunoz364 <57242034+angelmunoz364@users.noreply.github.com>
2019-11-25 21:53:40 -0600

    reportesv2

---
+++ controllers/ReporteProductoController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\controllers;
+
+
+use Yii;
+use app\models\Cliente;
+use app\models\ClienteSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use app\models\ReporteProductoSearch;
+
+
+/**
+ * ClienteController implements the CRUD actions for Cliente model.
+ */
+class ReporteProductoController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Cliente models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new ReporteProductoSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+}
