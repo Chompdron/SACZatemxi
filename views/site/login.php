f1c2f14fe40f009557a44ff3d762a0ecde44f689
mill120197 <53064397+mill120197@users.noreply.github.com>
2019-11-29 12:43:05 -0600

    Nuevos

--- views/site/login.php
+++ views/site/login.php
@@ -7,9 +7,11 @@
     use yii\helpers\Html;
     use yii\bootstrap\ActiveForm;
 
+
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+
 
 
 <img src="<?=Yii::getAlias('@web') ?>/Recursos/ZATEMXI%20Logo%20Dorado%20(2).png" id="logo">
@@ -33,20 +35,20 @@ $this->params['breadcrumbs'][] = $this->title;
         -->
     <?php $form = ActiveForm::begin([ 'id' => 'login-form', 'options'=>["class"=>"formulario"]]); ?>
 
+
         <h1>Inicia Sesion</h1>
         <div class="contenedor">
         <div class="input-contenedor">  
-            <i class="fas fa-envelope-square icon"></i>
+            
+           
         <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
             </div>
             
         <div class="input-contenedor">  
-            <i class="fas fa-key icon"></i>
+       
         <?= $form->field($model, 'password')->passwordInput() ?>
         </div>
             
             <input type="submit" value="Iniciar Sesion" class="button">
-            <p>¿Olvidaste la contraseña? <a class="link" href="recuperar.html">Recuperar</a></p>
-            <p>¿Aun no tienes una cuenta? <a class="link" href="registrarvista.html">Registrate</a></p>
-</div>
+            
     <?php ActiveForm::end(); ?>
