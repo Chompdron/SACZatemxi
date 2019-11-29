dd0d1139571d3b582b046f8adda0e340e22598e2
AndrésR <ndswii@live.com.mx>
2019-11-28 23:47:36 -0600

    Fix Insumo Search

--- models/InsumoSearch.php
+++ models/InsumoSearch.php
@@ -17,8 +17,9 @@ class InsumoSearch extends Insumo
     public function rules()
     {
         return [
-            [['InsumoID', 'UnidadPresentacionID', 'ProveedorID', 'Stock'], 'integer'],
-            [['Descripcion'], 'safe'],
+            [['InsumoID', 'Stock'], 'integer'],
+            [['ProveedorID', 'Descripcion'], 'string'],
+            [['UnidadPresentacionID'], 'safe'],
             [['PrecioXUnidad'], 'number'],
         ];
     }
@@ -41,7 +42,12 @@ class InsumoSearch extends Insumo
      */
     public function search($params)
     {
-        $query = Insumo::find();
+        $query = Insumo::find()->join('INNER JOIN',
+                                      'Proveedor',
+                                      'Insumo.ProveedorID = Proveedor.ProveedorID');
+        $query->join('INNER JOIN',
+                                      'UnidadPresentacion',
+                                      'Insumo.UnidadPresentacionID = UnidadPresentacion.UnidadPresentacionID');
 
         // add conditions that should always apply here
 
@@ -60,13 +66,11 @@ class InsumoSearch extends Insumo
         // grid filtering conditions
         $query->andFilterWhere([
             'InsumoID' => $this->InsumoID,
-            'UnidadPresentacionID' => $this->UnidadPresentacionID,
             'Stock' => $this->Stock,
             'PrecioXUnidad' => $this->PrecioXUnidad,
-            'ProveedorID' => $this->ProveedorID,
-        ]);
-
-        $query->andFilterWhere(['like', 'Descripcion', $this->Descripcion]);
+        ])->andFilterWhere(['like', 'Descripcion', $this->Descripcion]);
+        $query->andFilterWhere(['like', 'NombreComercial', $this->ProveedorID]);
+        $query->andFilterWhere(['like', 'UnidadPresentacion.Nombre', $this->UnidadPresentacionID]);
 
         return $dataProvider;
     }
